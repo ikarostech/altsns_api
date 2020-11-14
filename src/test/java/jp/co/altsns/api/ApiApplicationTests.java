2ddd7a460d8f62d314e02c8584da3d47470b7081
ikarostech <ikarostech@live.jp>
2020-11-14 21:10:38 +0900

    First Commit

---
+++ src/test/java/jp/co/altsns/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.co.altsns.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
